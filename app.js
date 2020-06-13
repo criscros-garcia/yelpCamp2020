6c9810aeed2019bbc23139c45b3bcacde22222d6
Cristian Garcia <eminemko@hotmail.com>
2020-06-13 16:14:30 -0500

    Set 'new'and 'post' routes to add new Campgrounds to the collection

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-let express = require("express");
+let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+var campgrounds = [
+  {name: "Winsconsin", url: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+  {name: "New York", url: "http://www.totalescape.com/GIFS/waterz/lakes/crowley/crowleylake.JPG"},
+  {name: "California", url: "https://image.shutterstock.com/image-photo/camping-under-stars-bonfire-tent-260nw-639243067.jpg"},
+]
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -9,12 +17,21 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
   
-  var campgrounds = [
-    {name: "Winsconsin", url: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
-    {name: "New York", url: "http://www.totalescape.com/GIFS/waterz/lakes/crowley/crowleylake.JPG"},
-    {name: "California", url: "https://image.shutterstock.com/image-photo/camping-under-stars-bonfire-tent-260nw-639243067.jpg"},
-  ]
-  res.render("index", {campgrounds:campgrounds});
+  
+  res.render('index', {campgrounds:campgrounds});
+});
+
+app.get('/campgrounds/new', function(req, res){
+  res.render('new');
+});
+
+app.post('/campgrounds', function(req, res){
+  let name =  req.body.name;
+  let url  =  req.body.image;
+  var newCampground = {name:name, url:url}
+  campgrounds.push(newCampground);
+
+  res.redirect('campgrounds');
 });
 
 
