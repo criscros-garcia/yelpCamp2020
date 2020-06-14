124cdf393dad52bfe9324596eb061da3c05c7ed7
Cristian Garcia <eminemko@hotmail.com>
2020-06-13 19:26:31 -0500

    Display campgrounds in cards format

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var campgrounds = [
   {name: "Winsconsin", url: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
   {name: "New York", url: "http://www.totalescape.com/GIFS/waterz/lakes/crowley/crowleylake.JPG"},
   {name: "California", url: "https://image.shutterstock.com/image-photo/camping-under-stars-bonfire-tent-260nw-639243067.jpg"},
+  {name: "Winsconsin", url: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+  {name: "New York", url: "http://www.totalescape.com/GIFS/waterz/lakes/crowley/crowleylake.JPG"},
+  {name: "California", url: "https://image.shutterstock.com/image-photo/camping-under-stars-bonfire-tent-260nw-639243067.jpg"}
 ]
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/css', express.static('css'));
 
 app.get("/", function(req, res){
   res.render("landing");
