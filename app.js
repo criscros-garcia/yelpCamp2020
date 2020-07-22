70dd98ba9c6535cd1d198da3589094e391a07667
Cristian Garcia <eminemko@hotmail.com>
2020-07-22 12:53:36 -0500

    Set 'login' routes

--- app.js
+++ app.js
@@ -219,6 +219,18 @@ app.post('/register', function(req, res){
   });    
 });
 
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', 
+{
+  successRedirect: '/campgrounds',
+  failureRedirect: '/login'
+}), function(req, res){
+
+});
+
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
   console.log("YelpCamp server has started!");
