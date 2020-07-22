122d5590ff4b472dab9ad107ed178059d59547e1
Cristian Garcia <eminemko@hotmail.com>
2020-07-22 18:24:58 -0500

    Refactor campground routes

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ let Campground = require('./models/campground');
 let Comment = require('./models/comment');
 let seedDB = require('./seed');
 
+let campgroundRoutes = require('./routes/campgrounds');
+
 mongoose.connect("mongodb://localhost/yelpCamp",  {useNewUrlParser: true , useUnifiedTopology: true });
 
 app.set("view engine", "ejs");
@@ -35,83 +37,13 @@ app.use(function(req, res, next){
   next();
 });
 
+app.use(campgroundRoutes);
+
 app.get("/", function(req, res){
   res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-  Campground.find({}, function(err, campgrounds){
-    if(err){
-      console.log(err);
-      console.log("Something went wrong");
-    }else{
-      res.render('index', {campgrounds:campgrounds});
-    }
-  });
-  
-});
 
-app.get('/campgrounds/new', function(req, res){
-  res.render('new');
-});
-
-app.post('/campgrounds', function(req, res){
-
-  let newCampground = req.body.campground;
-
-  Campground.create(newCampground, function(err, newCreated){
-    if(err){
-      console.log('Error creating a Campground¡');
-      console.log(err);
-    }else{
-      console.log("Campground created succesfully¡");
-      res.redirect('campgrounds');
-    }
-  });
-});
-
-app.get('/campgrounds/:id', function(req, res){
-  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCamp){
-    if(err){
-      console.log("Can't be found");
-    }else{
-      res.render('show', {campground:foundCamp});
-    }
-  });
-});
-
-app.get('/campgrounds/:id/edit', function(req, res){
-  Campground.findById(req.params.id, function(err, foundToUpdate){
-    if(err){
-      console.log('Could not be found to update¡'+ err);
-    }else{
-      res.render('edit', {campground: foundToUpdate})
-    }
-  });
-});
-
-app.put('/campgrounds/:id', function(req, res){
-  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updated){
-    if(err){
-      console.log("Error triying to Update");
-      res.redirect('index');
-    }else{
-      console.log("Succesfull update¡");
-      res.redirect('/campgrounds/'+req.params.id);
-    }
-  });
-});
-
-app.delete('/campgrounds/:id', function(req, res){
-  Campground.findByIdAndDelete(req.params.id, function(err){
-    if(err){
-      console.log("Error triying to eliminate¡");
-      res.redirect("/campgrounds/+req.params.id");
-    }else{
-      res.redirect('/campgrounds');
-    }
-  });
-});
 
 // COMMENTS ROUTES
 
---
+++ routes/campgrounds.js
@@ -0,0 +1,80 @@
+let express    = require('express');
+let router     = express.Router();
+let Campground = require('../models/campground');
+
+
+router.get("/campgrounds", function(req, res){
+  Campground.find({}, function(err, campgrounds){
+    if(err){
+      console.log(err);
+      console.log("Something went wrong");
+    }else{
+      res.render('index', {campgrounds:campgrounds});
+    }
+  });
+  
+});
+
+router.get('/campgrounds/new', function(req, res){
+  res.render('new');
+});
+
+router.post('/campgrounds', function(req, res){
+
+  let newCampground = req.body.campground;
+
+  Campground.create(newCampground, function(err, newCreated){
+    if(err){
+      console.log('Error creating a Campground¡');
+      console.log(err);
+    }else{
+      console.log("Campground created succesfully¡");
+      res.redirect('campgrounds');
+    }
+  });
+});
+
+router.get('/campgrounds/:id', function(req, res){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCamp){
+    if(err){
+      console.log("Can't be found");
+    }else{
+      res.render('show', {campground:foundCamp});
+    }
+  });
+});
+
+router.get('/campgrounds/:id/edit', function(req, res){
+  Campground.findById(req.params.id, function(err, foundToUpdate){
+    if(err){
+      console.log('Could not be found to update¡'+ err);
+    }else{
+      res.render('edit', {campground: foundToUpdate})
+    }
+  });
+});
+
+router.put('/campgrounds/:id', function(req, res){
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updated){
+    if(err){
+      console.log("Error triying to Update");
+      res.redirect('index');
+    }else{
+      console.log("Succesfull update¡");
+      res.redirect('/campgrounds/'+req.params.id);
+    }
+  });
+});
+
+router.delete('/campgrounds/:id', function(req, res){
+  Campground.findByIdAndDelete(req.params.id, function(err){
+    if(err){
+      console.log("Error triying to eliminate¡");
+      res.redirect("/campgrounds/+req.params.id");
+    }else{
+      res.redirect('/campgrounds');
+    }
+  });
+});
+
+module.exports = router;
